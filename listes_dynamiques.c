3c7065f5705c60c1f117b4ef11df0ba7fa71e331
richmartins <richard.martinstenorio@gmail.com>
2021-04-25 17:01:09 +0200

    [wip] all functions

--- listes_dynamiques.c
+++ listes_dynamiques.c
@@ -1,87 +1,190 @@
 #include "listes_dynamiques.h"
+#include <stdio.h>
 #include "stdlib.h"
-#include "stdint.h"
 
-Liste* initialiser() {
-   Liste* nouvelleListe = (Liste*) calloc(1, sizeof(Liste));
-   return nouvelleListe;
+Liste *initialiser() {
+    Liste *nouvelleListe = (Liste *) calloc(1, sizeof(Liste));
+    return nouvelleListe;
 }
 
-bool estVide(const Liste* liste) {
-  return liste->queue == NULL && liste->tete == NULL;
+bool estVide(const Liste *liste) {
+    return liste->queue == NULL && liste->tete == NULL;
 }
 
+size_t longueur(const Liste *liste) {
+    if (liste->queue == NULL && liste->tete == NULL) return 0;
+    if (liste->queue == liste->tete) return 1;
 
-size_t longueur(const Liste* liste) {
-	if (liste->queue == NULL && liste->tete == NULL) return 0;
-	if (liste->queue == liste->tete) return 1;
-   return (size_t) (liste->queue - liste->tete);
+    Element* actuel = liste->tete;
+
+    size_t compteur = 0;
+
+    while(actuel != NULL) {
+        compteur++;
+        actuel = actuel->suivant;
+    }
+
+    return compteur;
 }
 
-void afficher(const Liste* liste, Mode mode) {
+void afficher(const Liste *liste, Mode mode) {
+    Element* actuel;
 
+    actuel = mode == FORWARD ? liste->tete : liste->queue;
+
+    printf("[");
+    while (actuel != NULL) {
+        printf("%d", actuel->info);
+        if (mode == FORWARD) {
+            if (actuel != liste->queue) printf(", ");
+            actuel = actuel->suivant;
+        } else {
+            if (actuel != liste->tete) printf(", ");
+            actuel = actuel->precedent;
+        }
+    }
+    printf("]\n");
 }
 
-Status insererEnTete(Liste* liste, const Info* info) {
-   Element* el = (Element*) malloc(sizeof(Element));
-   if (el == NULL) {
-      return MEMOIRE_INSUFFISANTE;
-   }
-
-   *el = (Element) {
-      .info = *info,
-      .suivant = liste->tete,
-      .precedent = NULL
-   };
-
-	if (longueur(liste)) {
-		liste->tete->precedent = el;
-	} else {
-		// Liste vide
-		liste->queue = el;
-	}
-
-   liste->tete = el;
-
-   return OK;
-}
-
-Status insererEnQueue(Liste* liste, const Info* info) {
-    Element* el = (Element*) malloc(sizeof(Element));
+Status insererEnTete(Liste *liste, const Info *info) {
+    Element *el = (Element *) malloc(sizeof(Element));
     if (el == NULL) return MEMOIRE_INSUFFISANTE;
 
     *el = (Element) {
-        .info = *info,
-        .precedent = liste->queue,
-        .suivant = NULL
+            .info = *info,
+            .suivant = liste->tete,
+            .precedent = NULL
     };
 
-    liste->queue->suivant, liste->queue = el;
+    if (!estVide(liste)) {
+        liste->tete->precedent = el;
+    }
+    else {
+        // Liste vide
+        liste->queue = el;
+    }
+
+    liste->tete = el;
 
     return OK;
 }
 
-Status supprimerEnTete(Liste* liste, Info* info) {
-    liste->tete = liste->tete->suivant;
-    free(liste->tete);
-    // TODO: check a faire ???????
+Status insererEnQueue(Liste *liste, const Info *info) {
+    Element *el = (Element *) malloc(sizeof(Element));
+    if (el == NULL) return MEMOIRE_INSUFFISANTE;
+
+    *el = (Element) {
+            .info = *info,
+            .precedent = liste->queue,
+            .suivant = NULL
+    };
+
+    if (!estVide(liste)) {
+        liste->queue->suivant = el;
+    }
+    else {
+        liste->tete = el;
+    }
+    liste->queue = el;
 
     return OK;
 }
 
-Status supprimerEnQueue(Liste* liste, Info* info) {
+Status supprimerEnTete(Liste *liste, Info *info) {
+    if (estVide(liste)) return LISTE_VIDE;
+    Element *ancienneTete = liste->tete;
+    *info = ancienneTete->info;
 
+    if (longueur(liste) == 1) {
+        liste->tete = liste->queue = NULL;
+    }
+    else {
+        liste->tete = liste->tete->suivant;
+        liste->tete->precedent = NULL;
+    }
+
+    free(ancienneTete);
+
+    return OK;
 }
 
-void supprimerSelonCritere(Liste* liste,
-                           bool (*critere)(size_t position, const Info* info)) {
+Status supprimerEnQueue(Liste *liste, Info *info) {
+    if (estVide(liste)) return LISTE_VIDE;
+    Element *ancienneQueue = liste->queue;
+    *info = ancienneQueue->info;
 
+    if (longueur(liste) == 1) {
+        liste->tete = liste->queue = NULL;
+    }
+    else {
+        liste->queue = liste->queue->precedent;
+        liste->queue->suivant = NULL;
+    }
+
+    free(ancienneQueue);
+
+    return OK;
 }
 
-void vider(Liste* liste, size_t position) {
+void supprimerSelonCritere(Liste *liste,
+                           bool (*critere)(size_t position, const Info *info)) {
+    if (estVide(liste)) return;
 
+    Element* actuel = liste->tete;
+    size_t pos = 0;
+    while (actuel != NULL) {
+        Element* suivant = actuel->suivant;
+        if (critere(pos++, &actuel->info)) {
+            // delete current
+            Info tmpInfo;
+            if (actuel == liste->tete) {
+                supprimerEnTete(liste, &tmpInfo);
+            }
+            else if (actuel == liste->queue) {
+                supprimerEnQueue(liste, &tmpInfo);
+            }
+            else {
+                actuel->suivant->precedent = actuel->precedent;
+                actuel->precedent->suivant = actuel->suivant;
+
+                // TODO: BE SURE ABOUT THAT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                free(actuel);
+            }
+        }
+        actuel = suivant;
+    }
 }
 
-bool sontEgales(const Liste* liste1, const Liste* liste2) {
 
+void vider(Liste *liste, size_t position) {
+    if (longueur(liste) <= position) return;
+
+    Element* actuel = liste->tete;
+    size_t compteur = 0;
+
+    while(compteur < position) {
+        actuel = actuel->suivant;
+        compteur++;
+    }
+
+    while (liste->queue != actuel->precedent) {
+        Info tmpInfo;
+        supprimerEnQueue(liste, &tmpInfo);
+    }
+}
+
+bool sontEgales(const Liste *liste1, const Liste *liste2) {
+    if (longueur(liste1) != longueur(liste2)) return false;
+    if (estVide(liste1)) return true;
+
+    Element* actuel1 = liste1->tete;
+    Element* actuel2 = liste2->tete;
+    while (actuel1 != NULL) {
+        if(actuel1->info != actuel2->info) return false;
+
+        actuel1 = actuel1->suivant;
+        actuel2 = actuel2->suivant;
+    }
+
+    return true;
 }
--- main.c
+++ main.c
@@ -1,16 +1,74 @@
 #include "listes_dynamiques.h"
 #include <stdio.h>
 
+bool elPair(size_t position, const Info* info);
+
+
 int main() {
-   Liste* liste1Ptr = initialiser();
-   Liste* liste2Ptr = initialiser();
+    Liste *liste1Ptr = initialiser();
+    Liste *liste2Ptr = initialiser();
 
-   const Info info1 = 1;
-   const Info info2 = 2;
-   const Info info3 = 3;
+    const Info info1 = 1;
+    const Info info2 = 2;
+    const Info info3 = 3;
+    const Info info4 = 4;
 
-   insererEnTete(liste2Ptr, &info1);
-   insererEnTete(liste2Ptr, &info2);
-   insererEnTete(liste2Ptr, &info3);
-   return 0;
+    insererEnTete(liste2Ptr, &info1);
+    insererEnTete(liste2Ptr, &info2);
+    insererEnTete(liste2Ptr, &info3);
+
+    insererEnQueue(liste2Ptr, &info4);
+
+//    afficher(liste2Ptr, FORWARD);
+//    afficher(liste2Ptr, BACKWARD);
+
+//    insererEnQueue(liste1Ptr, &info1);
+//    insererEnQueue(liste1Ptr, &info2);
+//
+//    Info infoDeleted1;
+//    Info infoDeleted2;
+//    Info infoDeleted3;
+//
+//    longueur(liste2Ptr);
+//
+//    afficher(liste1Ptr, FORWARD);
+//    supprimerEnTete(liste1Ptr, &infoDeleted1);
+//    supprimerEnTete(liste1Ptr, &infoDeleted3);
+//    afficher(liste1Ptr, FORWARD);
+//
+//    //supprimerEnQueue(liste2Ptr, &infoDeleted2);
+//
+//
+    //supprimerSelonCritere(liste2Ptr, &elPair);
+    afficher(liste2Ptr, FORWARD);
+//    size_t uselessVarAgain = 2;
+//    vider(liste2Ptr,uselessVarAgain);
+//    afficher(liste2Ptr, FORWARD);
+
+
+    Liste* liste3Ptr = initialiser();
+    Liste* liste4Ptr = initialiser();
+    Info i1 = 1;
+    Info i2 = 2;
+    Info i3 = 3;
+    Info i4 = 4;
+
+
+    insererEnQueue(liste3Ptr, &i1);
+    insererEnQueue(liste3Ptr, &i2);
+    insererEnQueue(liste3Ptr, &i3);
+    insererEnQueue(liste3Ptr, &i4);
+
+    insererEnQueue(liste4Ptr, &i1);
+    insererEnQueue(liste4Ptr, &i2);
+    insererEnQueue(liste4Ptr, &i4);
+
+    printf("egale : %d", sontEgales(liste3Ptr, liste4Ptr));
+
+    return 0;
+}
+
+bool elPair(size_t position, const Info* info) {
+    if (*info == 3) return true;
+    return false;
 }
