4239c2639803b22f574711179f00a08c5db443d9
sarah jallon <sarah.jallon@gmail.com>
2021-04-26 17:53:02 +0200

    [REVIEW] programme de test

--- listes_dynamiques.c
+++ listes_dynamiques.c
@@ -1,190 +1,201 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : listes_dynamiques.c
+ Auteur(s)      : Jallon Sarah, Louis Hadrien, Martins Tenorio Richard V.
+ Date creation  : 26.04.2021
+
+ Description    : Implémentation des diverses fonctions permettant la création
+                  et l'édition d'une liste doublement chainée non circulaire
+
+ Remarque(s)    : Nous ne tenons pas compte du cas où le paramètre "liste" serait
+                  passé en tant que pointeur NULL.
+
+ Compilateur    : Mingw-w64 g++ 8.1.0
+                  Apple clang version 12.0.0 (clang-1200.0.32.29)
+ -----------------------------------------------------------------------------------
+*/
+
 #include "listes_dynamiques.h"
 #include <stdio.h>
 #include "stdlib.h"
 
 Liste *initialiser() {
-    Liste *nouvelleListe = (Liste *) calloc(1, sizeof(Liste));
-    return nouvelleListe;
+   Liste *nouvelleListe = (Liste *) calloc(1, sizeof(Liste));
+   return nouvelleListe;
 }
 
 bool estVide(const Liste *liste) {
-    return liste->queue == NULL && liste->tete == NULL;
+   return liste->queue == NULL && liste->tete == NULL;
 }
 
 size_t longueur(const Liste *liste) {
-    if (liste->queue == NULL && liste->tete == NULL) return 0;
-    if (liste->queue == liste->tete) return 1;
+   Element *actuel = liste->tete;
+   size_t compteur = 0;
 
-    Element* actuel = liste->tete;
+   while (actuel != NULL) {
+      compteur++;
+      actuel = actuel->suivant;
+   }
 
-    size_t compteur = 0;
-
-    while(actuel != NULL) {
-        compteur++;
-        actuel = actuel->suivant;
-    }
-
-    return compteur;
+   return compteur;
 }
 
 void afficher(const Liste *liste, Mode mode) {
-    Element* actuel;
+   Element *actuel;
 
-    actuel = mode == FORWARD ? liste->tete : liste->queue;
+   printf("[");
+   if (mode == FORWARD) {
+      actuel = liste->tete;
+      while (actuel != NULL) {
+         printf("%d", actuel->info);
+         if (actuel != liste->queue) printf(", ");
+         actuel = actuel->suivant;
+      }
+   } else if (mode == BACKWARD) {   //nous précisons la condition au cas où un
+      actuel = liste->queue;        // nouveau mode serait ajouté
+      while (actuel != NULL) {
+         printf("%d", actuel->info);
+         if (actuel != liste->tete) printf(", ");
+         actuel = actuel->precedent;
+      }
+   }
+   printf("]\n");
 
-    printf("[");
-    while (actuel != NULL) {
-        printf("%d", actuel->info);
-        if (mode == FORWARD) {
-            if (actuel != liste->queue) printf(", ");
-            actuel = actuel->suivant;
-        } else {
-            if (actuel != liste->tete) printf(", ");
-            actuel = actuel->precedent;
-        }
-    }
-    printf("]\n");
 }
 
 Status insererEnTete(Liste *liste, const Info *info) {
-    Element *el = (Element *) malloc(sizeof(Element));
-    if (el == NULL) return MEMOIRE_INSUFFISANTE;
+   Element *el = (Element *) malloc(sizeof(Element));
+   if (el == NULL) return MEMOIRE_INSUFFISANTE;
 
-    *el = (Element) {
-            .info = *info,
-            .suivant = liste->tete,
-            .precedent = NULL
-    };
+   *el = (Element) {
+      .info = *info,
+      .suivant = liste->tete,
+      .precedent = NULL
+   };
 
-    if (!estVide(liste)) {
-        liste->tete->precedent = el;
-    }
-    else {
-        // Liste vide
-        liste->queue = el;
-    }
+   if (!estVide(liste)) {
+      liste->tete->precedent = el;
+   } else {
+      // Liste vide
+      liste->queue = el;
+   }
 
-    liste->tete = el;
+   liste->tete = el;
 
-    return OK;
+   return OK;
 }
 
 Status insererEnQueue(Liste *liste, const Info *info) {
-    Element *el = (Element *) malloc(sizeof(Element));
-    if (el == NULL) return MEMOIRE_INSUFFISANTE;
+   Element *el = (Element *) malloc(sizeof(Element));
+   if (el == NULL) return MEMOIRE_INSUFFISANTE;
 
-    *el = (Element) {
-            .info = *info,
-            .precedent = liste->queue,
-            .suivant = NULL
-    };
+   *el = (Element) {
+      .info = *info,
+      .precedent = liste->queue,
+      .suivant = NULL
+   };
 
-    if (!estVide(liste)) {
-        liste->queue->suivant = el;
-    }
-    else {
-        liste->tete = el;
-    }
-    liste->queue = el;
+   if (!estVide(liste)) {
+      liste->queue->suivant = el;
+   } else {
+      liste->tete = el;
+   }
+   liste->queue = el;
 
-    return OK;
+   return OK;
 }
 
 Status supprimerEnTete(Liste *liste, Info *info) {
-    if (estVide(liste)) return LISTE_VIDE;
-    Element *ancienneTete = liste->tete;
-    *info = ancienneTete->info;
+   if (estVide(liste)) return LISTE_VIDE;
+   Element *ancienneTete = liste->tete;
+   *info = ancienneTete->info;
 
-    if (longueur(liste) == 1) {
-        liste->tete = liste->queue = NULL;
-    }
-    else {
-        liste->tete = liste->tete->suivant;
-        liste->tete->precedent = NULL;
-    }
+   if (longueur(liste) == 1) {
+      liste->tete = liste->queue = NULL;
+   } else {
+      liste->tete = liste->tete->suivant;
+      liste->tete->precedent = NULL;
+   }
 
-    free(ancienneTete);
+   free(ancienneTete);
 
-    return OK;
+   return OK;
 }
 
 Status supprimerEnQueue(Liste *liste, Info *info) {
-    if (estVide(liste)) return LISTE_VIDE;
-    Element *ancienneQueue = liste->queue;
-    *info = ancienneQueue->info;
+   if (estVide(liste)) return LISTE_VIDE;
+   Element *ancienneQueue = liste->queue;
+   *info = ancienneQueue->info;
 
-    if (longueur(liste) == 1) {
-        liste->tete = liste->queue = NULL;
-    }
-    else {
-        liste->queue = liste->queue->precedent;
-        liste->queue->suivant = NULL;
-    }
+   if (longueur(liste) == 1) {
+      liste->tete = liste->queue = NULL;
+   } else {
+      liste->queue = liste->queue->precedent;
+      liste->queue->suivant = NULL;
+   }
 
-    free(ancienneQueue);
+   free(ancienneQueue);
 
-    return OK;
+   return OK;
 }
 
 void supprimerSelonCritere(Liste *liste,
                            bool (*critere)(size_t position, const Info *info)) {
-    if (estVide(liste)) return;
+   if (estVide(liste)) return;
 
-    Element* actuel = liste->tete;
-    size_t pos = 0;
-    while (actuel != NULL) {
-        Element* suivant = actuel->suivant;
-        if (critere(pos++, &actuel->info)) {
-            // delete current
-            Info tmpInfo;
-            if (actuel == liste->tete) {
-                supprimerEnTete(liste, &tmpInfo);
-            }
-            else if (actuel == liste->queue) {
-                supprimerEnQueue(liste, &tmpInfo);
-            }
-            else {
-                actuel->suivant->precedent = actuel->precedent;
-                actuel->precedent->suivant = actuel->suivant;
+   Element *actuel = liste->tete;
+   size_t pos = 0;
+   while (actuel != NULL) {
+      Element *suivant = actuel->suivant;
+      if (critere(pos++, &actuel->info)) {
+         // delete current
+         Info tmpInfo;
+         if (actuel == liste->tete) {
+            supprimerEnTete(liste, &tmpInfo);
+         } else if (actuel == liste->queue) {
+            supprimerEnQueue(liste, &tmpInfo);
+         } else {
+            actuel->suivant->precedent = actuel->precedent;
+            actuel->precedent->suivant = actuel->suivant;
 
-                // TODO: BE SURE ABOUT THAT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-                free(actuel);
-            }
-        }
-        actuel = suivant;
-    }
+            // TODO: BE SURE ABOUT THAT <<<<<<<< add comments
+            free(actuel);
+         }
+      }
+      actuel = suivant;
+   }
 }
 
 
 void vider(Liste *liste, size_t position) {
-    if (longueur(liste) <= position) return;
+   if (longueur(liste) <= position) return;
 
-    Element* actuel = liste->tete;
-    size_t compteur = 0;
+   Element *actuel = liste->tete;
+   size_t compteur = 0;
 
-    while(compteur < position) {
-        actuel = actuel->suivant;
-        compteur++;
-    }
+   while (compteur < position) {
+      actuel = actuel->suivant;
+      compteur++;
+   }
 
-    while (liste->queue != actuel->precedent) {
-        Info tmpInfo;
-        supprimerEnQueue(liste, &tmpInfo);
-    }
+   while (liste->queue != actuel->precedent) {
+      Info tmpInfo;
+      supprimerEnQueue(liste, &tmpInfo);
+   }
 }
 
 bool sontEgales(const Liste *liste1, const Liste *liste2) {
-    if (longueur(liste1) != longueur(liste2)) return false;
-    if (estVide(liste1)) return true;
+   if (longueur(liste1) != longueur(liste2)) return false;
+   if (estVide(liste1)) return true;
 
-    Element* actuel1 = liste1->tete;
-    Element* actuel2 = liste2->tete;
-    while (actuel1 != NULL) {
-        if(actuel1->info != actuel2->info) return false;
+   Element *actuel1 = liste1->tete;
+   Element *actuel2 = liste2->tete;
+   while (actuel1 != NULL) {
+      if (actuel1->info != actuel2->info) return false;
 
-        actuel1 = actuel1->suivant;
-        actuel2 = actuel2->suivant;
-    }
+      actuel1 = actuel1->suivant;
+      actuel2 = actuel2->suivant;
+   }
 
-    return true;
+   return true;
 }
--- main.c
+++ main.c
@@ -1,10 +1,28 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : main.c
+ Auteur(s)      : Jallon Sarah, Louis Hadrien, Martins Tenorio Richard V.
+ Date creation  : 26.04.2021
+
+ Description    : Programme testant le bon fonctionnnement des fonctions de la
+                  librairie listes_dynamiques.h
+
+ Remarque(s)    : -
+
+ Compilateur    : Mingw-w64 g++ 8.1.0
+                  Apple clang version 12.0.0 (clang-1200.0.32.29)
+ -----------------------------------------------------------------------------------
+*/
+
 #include "listes_dynamiques.h"
 #include <stdio.h>
 
-bool elPair(size_t position, const Info *info);
+bool elEstTrois(size_t position, const Info *info);
+bool elEstPair(size_t position, const Info *info);
+
 
 int main() {
-   //-------------------------------------------------------
+   //-------------------------------------------------------------------------------
    //création des variables infos
 
    const Info info1 = 1;
@@ -13,11 +31,12 @@ int main() {
    const Info info4 = 4;
    const Info info5;       // servira pour la fonction de suppression
 
-   //-------------------------------------------------------
+   //-------------------------------------------------------------------------------
    //initialisation listes
 
    Liste *liste1Ptr = initialiser();
    Liste *liste2Ptr = initialiser();
+
 
    printf("Initialisation liste1 : ");
    afficher(liste1Ptr, FORWARD);
@@ -25,7 +44,7 @@ int main() {
    printf("Initialisation liste2 : ");
    afficher(liste2Ptr, FORWARD);
 
-   //-------------------------------------------------------
+   //-------------------------------------------------------------------------------
    //remplissage des listes
 
    printf("\nInsertion de 4 elements en tete de liste2  : ");
@@ -43,15 +62,17 @@ int main() {
 
    afficher(liste2Ptr,FORWARD);
 
-   //-------------------------------------------------------
+   //-------------------------------------------------------------------------------
    //tests vide et longueur
 
    printf("\nLa liste1 est-elle vide ? : %s, elle a une longueur de %zu\n",
-          estVide(liste1Ptr) ? "oui" : "non",longueur(liste1Ptr));
+          estVide(liste1Ptr) ? "oui" : "non",
+          longueur(liste1Ptr));
    printf("La liste2 est-elle vide ? : %s, elle a une longueur de %zu\n",
-          estVide(liste2Ptr) ? "oui" : "non",longueur(liste2Ptr));
+          estVide(liste2Ptr) ? "oui" : "non",
+          longueur(liste2Ptr));
 
-   //-------------------------------------------------------
+   //-------------------------------------------------------------------------------
    //tests fonctions d'affichage
 
    printf("\nListe2 en mode FORWARD  : ");
@@ -60,13 +81,101 @@ int main() {
    printf("Liste2 en mode BACKWARD : ");
    afficher(liste2Ptr,BACKWARD);
 
-   //test fonctions de vidage
+   //-------------------------------------------------------------------------------
+   //test fonctions de supression
+
+   printf("\nListe2 etat acutel : ");
+   afficher(liste2Ptr,FORWARD);
+
+   supprimerEnTete(liste2Ptr,&info5);
+
+   printf("\nListe2 apres supression en tete : ");
+   afficher(liste2Ptr,FORWARD);
+   printf("Info supprimee: %d\n", info5);
+
+   supprimerEnQueue(liste2Ptr, &info5);
+
+   printf("\nListe2 apres supression en queue : ");
+   afficher(liste2Ptr,FORWARD);
+   printf("Info supprimee: %d\n", info5);
+
+   supprimerSelonCritere(liste2Ptr,elEstTrois);
+   printf("\nListe2 apres supression des occurences de 3 : ");
+   afficher(liste2Ptr,FORWARD);
+
+   supprimerSelonCritere(liste2Ptr,elEstPair);
+   printf("Liste2 apres supression des nombres pairs : ");
+   afficher(liste2Ptr,FORWARD);
+
+   //-------------------------------------------------------------------------------
+   //test vidage + égalité
+
+   vider(liste2Ptr,0);
+   printf("Liste2 apres avoir ete videe depuis la position 0 : ");
+   afficher(liste2Ptr,FORWARD);
+
+   printf("\nEtat actuel de Liste1 : ");
+   afficher(liste1Ptr,FORWARD);
+   printf("\nLes listes 1 et 2 sont-elles egales ? %s\n",
+          sontEgales(liste2Ptr,liste1Ptr) ? "oui" : "non");
+
+
+   insererEnTete(liste2Ptr, &info1);
+   insererEnTete(liste2Ptr, &info2);
+   insererEnTete(liste2Ptr, &info3);
+   insererEnTete(liste2Ptr, &info4);
+   insererEnTete(liste2Ptr, &info1);
+   insererEnTete(liste2Ptr, &info2);
+   insererEnTete(liste2Ptr, &info3);
+   insererEnTete(liste2Ptr, &info4);
+
+   insererEnTete(liste1Ptr, &info1);
+   insererEnTete(liste1Ptr, &info2);
+   insererEnTete(liste1Ptr, &info3);
+   insererEnTete(liste1Ptr, &info4);
+   insererEnTete(liste1Ptr, &info1);
+   insererEnTete(liste1Ptr, &info2);
+   insererEnTete(liste1Ptr, &info3);
+   insererEnTete(liste1Ptr, &info4);
+
+   printf("\nEtat actuel de liste1 : ");
+   afficher(liste1Ptr,FORWARD);
+
+   printf("Etat actuel de liste2 : ");
+   afficher(liste2Ptr,FORWARD);
+
+   printf("\nLes listes 1 et 2 sont-elles egales ? %s\n",
+          sontEgales(liste2Ptr,liste1Ptr) ? "oui" : "non");
+
+   printf("\nVidage de liste2 depuis la position 5: ");
+   vider(liste2Ptr, 5);
+   afficher(liste2Ptr,FORWARD);
+
+   printf("\nLes listes 1 et 2 sont-elles egales ? %s\n",
+          sontEgales(liste2Ptr,liste1Ptr) ? "oui" : "non");
+
+   printf("\nVidage complet de liste1 et liste2 : ");
+   vider(liste2Ptr,0);
+   vider(liste1Ptr,0);
+
+   printf("\nEtat actuel de liste1 : ");
+   afficher(liste1Ptr,FORWARD);
+
+   printf("Etat actuel de liste2 : ");
+   afficher(liste2Ptr,FORWARD);
+
+   printf("\nFin de programme");
 
 
    return 0;
 }
 
-bool elPair(size_t position, const Info *info) {
+bool elEstTrois(size_t position, const Info *info) {
    if (*info == 3) return true;
    return false;
 }
+bool elEstPair(size_t position, const Info *info) {
+   if (*info % 2 == 0) return true;
+   return false;
+}
+
